c0234a19104050b9cbea51e75c0c5ba07ef31447
Conanami <edogawaconanami@proton.me>
2022-12-12 14:45:48 -0400

    redux

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,16 @@
 import "./category.styles.scss";
 import { useParams } from "react-router-dom";
-import { useContext, useEffect, useState, Fragment } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { useEffect, useState, Fragment } from "react";
+
 import ProductCard from "../../components/product-card/product-card.component";
+import { setCategoriesMap } from "../../store/categories/category.action";
+import { useSelector } from "react-redux";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 const Category = () => {
   //console.log(" i am category");
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
   useEffect(() => {
     setProducts(categoriesMap[category]);
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,9 +2,11 @@ import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { useContext } from "react";
-import { UserContext } from "../../contexts/user.context";
+
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -15,8 +17,7 @@ import {
   NavLink,
 } from "./navigation.styles";
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  //console.log(currentUser);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
